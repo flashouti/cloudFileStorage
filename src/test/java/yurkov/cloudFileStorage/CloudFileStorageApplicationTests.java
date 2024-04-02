0da1e15590f50bcbf30274047f50fef0c81d3404
Alexey Yurkov <yessirskill@yandex.ru>
2024-04-02 16:07:04 +0500

    Start Project

---
+++ src/test/java/yurkov/cloudFileStorage/CloudFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package yurkov.cloudFileStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
